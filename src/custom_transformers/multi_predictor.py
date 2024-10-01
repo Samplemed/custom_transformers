1bc367460705c2f37693034a02cf841fcbfa7523
leouchoapedreira <97109238+leouchoapedreira@users.noreply.github.com>
2024-10-01 10:00:02 -0300

    Feat: `predict_proba` returns fine-grained probabilities for binary classification (#5)
    * feat: `predict_proba` can return only 0 class pred
    
    * feat: binary classification specific prob returns
    
    * docs: better signatures and docstrings
    
    ---------
    
    Co-authored-by: Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>

--- src/custom_transformers/multi_predictor.py
+++ src/custom_transformers/multi_predictor.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from pandas import DataFrame, merge
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
@@ -33,27 +33,39 @@ class MultiPredictor(BaseEstimator, ClassifierMixin):
         predictions_dict = {}
         for client_coverage in self.vars_and_pipe_dict:
             predictions_dict[client_coverage] = (
-                self.vars_and_pipe_dict[client_coverage]
-                .predict(X)
-                .ravel()
-                .tolist()
+                self.vars_and_pipe_dict[client_coverage].predict(X).ravel().tolist()
             )
 
         return predictions_dict
 
-    def predict_proba(self, X) -> dict[str, list[float]]:
+    def predict_proba(
+        self, X, bin_classif_return_classes_idx: Optional[Literal[0, 1]] = None
+    ) -> dict[str, list[float]]:
         """
         predicts proba
+
+        Args:
+
+            - `bin_classif_return_classes_idx`: mostly usefull for binary classification,
+            returns either the 0 (P(X=0)) probability class, 1 probability class (P(X=1))
+            or all probability classes predictions.
         """
+
+        assert (
+            bin_classif_return_classes_idx == 0
+            or bin_classif_return_classes_idx == 1
+            or bin_classif_return_classes_idx is None
+        ), f"`return_classes_idx` should either 0, 1 or None, not {bin_classif_return_classes_idx}"
 
         predictions_dict = {}
         for client_coverage in self.vars_and_pipe_dict:
-            predictions_dict[client_coverage] = (
-                self.vars_and_pipe_dict[client_coverage]
-                .predict_proba(X)
-                .ravel()
-                .tolist()
-            )
+            probas = self.vars_and_pipe_dict[client_coverage].predict_proba(X)
+            if bin_classif_return_classes_idx == 0:
+                predictions_dict[client_coverage] = probas[:, 0].ravel().tolist()
+            elif bin_classif_return_classes_idx == 1:
+                predictions_dict[client_coverage] = probas[:, 1].ravel().tolist()
+            else:
+                predictions_dict[client_coverage] = probas.ravel().tolist()
 
         return predictions_dict
 
@@ -67,7 +79,7 @@ class JoinTransformer(BaseEstimator, TransformerMixin):
         on: str,
         other_df: DataFrame,
         how: Literal["inner", "left", "right"],
-        drop_key: bool = True
+        drop_key: bool = True,
     ):
         """todo"""
         self.on = on
@@ -128,4 +140,29 @@ if __name__ == "__main__":
         }
     )
 
-    print(multi_predictor.predict_proba(X))
+    single_row = X[[50]]
+
+    print(
+        f"""
+        Probability prediction for all classes:
+        { multi_predictor.predict_proba(single_row) }
+        """
+    )
+    print(
+        f"""
+        Probability prediction for 0 class:
+        {multi_predictor.predict_proba(single_row, bin_classif_return_classes_idx=0)}
+        """
+    )
+    print(
+        f"""
+        Probability prediction for 1 class:
+        {multi_predictor.predict_proba(single_row, bin_classif_return_classes_idx=1)}
+        """
+    )
+    print(
+        f"""
+        Class predictions:
+        {multi_predictor.predict(single_row)}
+        """
+    )
